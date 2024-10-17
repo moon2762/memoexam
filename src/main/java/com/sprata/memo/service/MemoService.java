6e3b941f19d2a5397770217428bb34b4d900ba50
vanom <ji_yeon-_-@nate.com>
2024-10-17 16:37:29 +0900

    계체 중복 생성 문제 해결

--- src/main/java/com/sprata/memo/service/MemoService.java
+++ src/main/java/com/sprata/memo/service/MemoService.java
@@ -10,70 +10,53 @@ import java.util.List;
 
 public class MemoService {
 
-    private final JdbcTemplate jdbcTemplate;
+    private final MemoRepository memoRepository;
 
     public MemoService(JdbcTemplate jdbcTemplate) {
-        this.jdbcTemplate = jdbcTemplate;
+        this.memoRepository = new MemoRepository(jdbcTemplate);
     }
 
-    public MemoResponseDto createMemo(MemoRequestDto requestDto){
-
+    public MemoResponseDto createMemo(MemoRequestDto requestDto) {
         // RequestDto -> Entity
         Memo memo = new Memo(requestDto);
-
         // DB 저장
-        MemoRepository memoRepository = new MemoRepository(jdbcTemplate);
         memoRepository.save(memo);
-
         // Entity -> ResponseDto
-        MemoResponseDto memoResponseDto = new MemoResponseDto(memo);
-
-        return memoResponseDto;
-
+        return new MemoResponseDto(memo);
     }
 
     public List<MemoResponseDto> getMemos() {
-
         // DB 조회
-        MemoRepository memoRepository = new MemoRepository(jdbcTemplate);
         return memoRepository.findAll();
     }
 
     public Long updateMemos(Long id, MemoRequestDto requestDto) {
-
-        MemoRepository memoRepository = new MemoRepository(jdbcTemplate);
         // 해당 메모가 db에 존재하는지 확인
         Memo memo = memoRepository.findById(id);
-
         if (memo != null) {
             // memo 내용 수정
             memoRepository.update(id, requestDto);
-
             return id;
         } else {
             throw new IllegalArgumentException("선택한 메모는 존재하지 않습니다.");
         }
-
     }
 
 
-
     public Long deleteMemos(Long id) {
-        MemoRepository memoRepository = new MemoRepository(jdbcTemplate);
-
         // 해당 메모가 DB에 존재하는지 확인
         Memo memo = memoRepository.findById(id);
-        if(memo != null) {
+        if (memo != null) {
             memoRepository.delete(id);
             return id;
         } else {
             throw new IllegalArgumentException("선택한 메모는 존재하지 않습니다.");
         }
-
     }
 
 
-    }
+
+}
 
 
 
