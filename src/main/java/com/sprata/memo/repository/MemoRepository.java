cc3824f48208e462ce7d0dc3eee8378be591461a
vanom <ji_yeon-_-@nate.com>
2024-10-23 00:33:58 +0900

    SpringBoot의 JPA

--- src/main/java/com/sprata/memo/repository/MemoRepository.java
+++ src/main/java/com/sprata/memo/repository/MemoRepository.java
@@ -3,11 +3,13 @@ package com.sprata.memo.repository;
 import com.sprata.memo.dto.MemoRequestDto;
 import com.sprata.memo.dto.MemoResponseDto;
 import com.sprata.memo.entity.Memo;
+import jakarta.persistence.EntityManager;
 import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.jdbc.core.RowMapper;
 import org.springframework.jdbc.support.GeneratedKeyHolder;
 import org.springframework.jdbc.support.KeyHolder;
 import org.springframework.stereotype.Repository;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
@@ -103,5 +105,16 @@ public class MemoRepository {
             }
         }, id);
     }
+
+    @Transactional // 자식 메소드
+    public Memo createMemo(EntityManager em) {
+        Memo memo = em.find(Memo.class, 1);
+        memo.setUsername("Robbie");
+        memo.setContents("@Transactional 전파 테스트 중!");
+
+        System.out.println("createMemo 메서드 종료");
+        return memo;
+    }
+
 
 }
